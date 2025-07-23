701850837f3874c1db10100bd3fde72266f80bd3
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-23 21:04:47 +0300

    Add files via upload

---
+++ netology_task_8_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+namespace sum {
+	int calc(int x, int y) {
+		return x + y;
+	}
+}
+
+namespace sub {
+	int calc(int x, int y) {
+		return x - y;
+	}
+}
+
+namespace mult {
+	int calc(int x, int y) {
+		return x * y;
+	}
+}
+
+namespace divide {
+	int calc(int x, int y) {
+		return x / y;
+	}
+}
+
+int main() {
+	int x{ 6 };
+	int y{ 10 };
+	std::cout << "x = " << x << ", y = " << y << std::endl;
+	std::cout << "Summ: " << sum::calc(x, y) << std::endl;;
+	std::cout << "Substraction: " << sub::calc(x, y) << std::endl;
+	std::cout << "Multiplication: " << mult::calc(x, y) << std::endl;
+	std::cout << "Division: " << divide::calc(x, y) << std::endl;
+}
