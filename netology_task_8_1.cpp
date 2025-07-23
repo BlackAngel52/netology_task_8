11fc619c749b1c02258b4276b978df16b198c394
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-23 20:51:01 +0300

    Add files via upload

---
+++ netology_task_8_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+void counting_function() {
+    static int count{1};
+    std::cout << "Number of times function has been called: " << count << std::endl;
+    ++count;
+}
+
+
+int main()
+{
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
